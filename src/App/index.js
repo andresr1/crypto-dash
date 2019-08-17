14b82a5710d7c33f7d0d4fcbc38e428e7144ff15
andres <andresr1690@gmail.com>
2019-08-16 18:20:26 -0700

    File Structure Update
    See previous commit.

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AppLayout from './AppLayout';
+
+function App() {
+    return (
+      <AppLayout>
+        <h1>Initial Setup & AppLayout</h1>
+      </AppLayout>
+    );
+}
+
+export default App;
